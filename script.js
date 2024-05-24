8a8be549cb05204972deb1765c40b03d0da27a32
webploy99 <webploy99@gmail.com>
2024-05-24 13:26:20 +0700

    update commit

--- script.js
+++ script.js
@@ -28,3 +28,45 @@ function OpenNav(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
     }
+
+    // splide slide show
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        slidesPerView: 3,
+        // effect: "cube",
+        grabCursor: true,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            320: {
+            slidesPerView: 1,
+            },
+            480: {
+            slidesPerView: 3,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                const currentSlide = s.slides[s.activeIndex];
+                const progressBar = currentSlide.querySelector(".progressBar");
+                if (progressBar) {
+                    const widthPercentage = parseFloat(progress) * 100;
+                    progressBar.style.width = widthPercentage + "%";
+                }
+            },
+        },
+    });
